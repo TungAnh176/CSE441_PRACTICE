fc28391f7ec7a506c2c2e952c863443a93647c4a
TungAnh176 <tungganhh312@gmail.com>
2024-09-27 11:41:49 +0700

    init

---
+++ EX07/app/src/main/java/com/example/switching/ChildActivity.java
@@ -0,0 +1,23 @@
+package com.example.switching;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Button btn2 = (Button) findViewById(R.id.btnBack);
+
+    }
+}
