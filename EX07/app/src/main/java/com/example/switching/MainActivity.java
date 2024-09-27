575e88cf860b8edb4a749e3fc93fadacd7f730dd
TungAnh176 <tungganhh312@gmail.com>
2024-09-27 11:46:57 +0700

    update button parent

--- EX07/app/src/main/java/com/example/switching/MainActivity.java
+++ EX07/app/src/main/java/com/example/switching/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Button btn1 = (Button) findViewById(R.id.btnNext);
-
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this,ChildActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
