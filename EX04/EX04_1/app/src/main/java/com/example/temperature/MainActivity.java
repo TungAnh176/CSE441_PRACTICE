17eb992728563ddc554bc568191c1249a9fe8d85
TungAnh176 <tungganhh312@gmail.com>
2024-10-01 10:51:46 +0700

    init GUI

---
+++ EX04/EX04_1/app/src/main/java/com/example/temperature/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.temperature;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtC, edtF;
+    Button btnCF, btnFC, btnClear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCF = findViewById(R.id.btnCF);
+        btnFC = findViewById(R.id.btnFC);
+        btnClear = findViewById(R.id.btnClear);
+        
+    }
+}
