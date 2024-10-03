c1aa57902dc5355910054643c5d7ad63788952e4
TungAnh176 <tungganhh312@gmail.com>
2024-10-03 11:25:38 +0700

    done

--- EX06/app/src/main/java/com/example/infomation/MainActivity.java
+++ EX06/app/src/main/java/com/example/infomation/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.infomation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,6 +41,92 @@ public class MainActivity extends AppCompatActivity {
         btnGui = findViewById(R.id.btn_gui);
         group = findViewById(R.id.group);
 
-        
+        btnGui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doShowInformation();
+            }
+        });
     }
+    private void doShowInformation() {
+        String ten = edtHoTen.getText().toString();
+        ten = ten.trim();
+        if (ten.length() < 3) {
+            edtHoTen.requestFocus();
+            edtHoTen.selectAll();
+            Toast.makeText(this, "Họ tên không hợp lệ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String cccd = edtCCCD.getText().toString();
+        cccd = cccd.trim();
+        if (cccd.length() != 9) {
+            edtCCCD.requestFocus();
+            edtCCCD.selectAll();
+            Toast.makeText(this, "CCCD không hợp lệ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int checkedId = group.getCheckedRadioButtonId();  // Lấy ID của RadioButton được chọn
+        String bang = "";
+        if (checkedId != -1) {
+            RadioButton radioButton = findViewById(checkedId);
+            bang = radioButton.getText().toString();  // Lấy text của RadioButton
+        } else {
+            Toast.makeText(this, "Bạn chưa chọn bằng cấp", Toast.LENGTH_SHORT).show();
+            return;  // Ngăn không cho tiếp tục nếu chưa chọn bằng cấp
+        }
+
+        String sothich = "";
+        if (ckDocBao.isChecked()) {
+            sothich += ckDocBao.getText().toString() + "\n";
+        }
+        if (ckDocSach.isChecked()) {
+            sothich += ckDocSach.getText().toString() + "\n";
+        }
+        if (ckDocCode.isChecked()) {
+            sothich += ckDocCode.getText().toString() + "\n";
+        }
+        String bosung = edtBosung.getText().toString();
+        bosung = bosung.trim();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thông tin cá nhân");
+        builder.setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        String msg = ten +"\n";
+        msg += cccd + "\n";
+        msg += bang + "\n";
+        msg += sothich;
+        msg += "----------------\n";
+        msg += "THÔNG TIN BỔ SUNG: \n";
+        msg += bosung+ "\n";
+        builder.setMessage(msg);
+        builder.create().show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+        b.setTitle("Question");
+        b.setMessage("Are you want exit?");
+        b.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        b.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        b.create().show();
+    }
+
 }
