c5ec2ca530bc94094ee9c2d0ee6b9c2c02ae2a67
TungAnh176 <tungganhh312@gmail.com>
2024-10-03 10:41:32 +0700

    findid

--- EX06/app/src/main/java/com/example/infomation/MainActivity.java
+++ EX06/app/src/main/java/com/example/infomation/MainActivity.java
@@ -27,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtHoTen = findViewById(R.id.edt_ho_ten);
+        edtCCCD = findViewById(R.id.edt_cccd);
+        edtBosung = findViewById(R.id.edt_bosung);
+        ckDocBao = findViewById(R.id.ck_docbao);
+        ckDocSach = findViewById(R.id.ck_docsach);
+        ckDocCode = findViewById(R.id.ck_doccode);
+        btnGui = findViewById(R.id.btn_gui);
+        group = findViewById(R.id.group);
+
+        
     }
 }
