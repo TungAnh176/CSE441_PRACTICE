35c19856c6b991a45635a2f19ea5f9f51ac2e5c3
TungAnh176 <tungganhh312@gmail.com>
2024-10-03 10:38:50 +0700

    create GUI

---
+++ EX06/app/src/main/java/com/example/infomation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.infomation;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtHoTen, edtCCCD, edtBosung;
+    CheckBox ckDocBao, ckDocSach, ckDocCode;
+    Button btnGui;
+    RadioGroup group;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
