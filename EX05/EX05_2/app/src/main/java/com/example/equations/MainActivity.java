8e72967c48462a2e37cb2dc6f1b64659b2f6df97
TungAnh176 <tungganhh312@gmail.com>
2024-10-03 10:03:53 +0700

    create GUI

--- EX05/EX05_2/app/src/main/java/com/example/equations/MainActivity.java
+++ EX05/EX05_2/app/src/main/java/com/example/equations/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.equations;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB, edtC, edtKQ;
+    Button btnPT, btnTT, btnThoat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtA = findViewById(R.id.edt_a);
+        edtB = findViewById(R.id.edt_b);
+        edtC = findViewById(R.id.edt_c);
+        edtKQ = findViewById(R.id.edt_kq);
+        btnPT = findViewById(R.id.btn_pt);
+        btnTT = findViewById(R.id.btn_tt);
+        btnThoat = findViewById(R.id.btn_thoat);
+        
     }
 }
